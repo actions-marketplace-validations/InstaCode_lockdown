1d1cd87e97946748c94be7433a9ef8b7a4c7e841
Jason Edstrom <JasonE@magenic.com>
2020-03-04 21:12:49 -0600

    verifyuser logic and tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const collaborators = require('./verifyUser');
+const verifyUser = require('./verifyUser');
 
 const username = github.context.actor;
 
@@ -8,14 +8,13 @@ async function run() {
     core.debug("Verifying that username ${username} is approved for running builds");
     const usernames = core.getInput('users');
     core.info(usernames);
-    const verified = collaborators.
+    const verified =  verifyUser(username);
 
     if (!verified)
     {
         core.setFailed("User " + username + "is not approved to execute builds on the pipeline");
     }
-
 }
-run()
+run();
 
 
